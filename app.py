d07e932b2ba47771bbb9585c6179ed344d63f195
Adamkharchaf <adamkharchaf2010@gmail.com>
2025-04-19 19:02:33 +0100

    All the modifications 19/4/2025

--- app.py
+++ app.py
@@ -11,26 +11,41 @@ class BaseModel(Model):
 
 
 
+class category(BaseModel):
+    name = CharField(max_length=100 , unique=True)
+    date_added = DateField(formats="%Y-%m-%d" ,default=datetime.now)
+
 class User(BaseModel):
+    #country list:
     username = CharField(unique=True)
+    age = IntegerField()
+    country = CharField()
+    join_date = DateTimeField(formats="%Y-%m-%d %H:%M:%S" , default=datetime.now)
+
+
+
+class comment(BaseModel):
+    content= CharField(max_length=255, null=False)
+    created_at = DateField(formats="%Y-%m-%d" , default=datetime.now)
+    is_published = BooleanField(default=False)
+    user= ForeignKeyField(model=User, backref="comments")
+
 
 
 class Post(BaseModel):
     title = CharField()
     content = TextField()
     created_at = DateTimeField(formats="%Y-%m-%d %H:%M:%S", default=datetime.now)
-    user_id= ForeignKeyField(User , backref='posts')
+    user= ForeignKeyField(User , backref='posts')
+    category = ForeignKeyField(model=category , backref="posts")
+
     
 
 # Entry point 
 if __name__== '__main__':
-    #db.connect()
-    #db.create_tables([User,Post])
+   db.connect()
+   db.create_tables([User,comment,category,Post])
 
-    #ajouter un nouvel utilisateur
-    #adam = User.create(username='anassitos')
     
-    anas=User.get(User.username=='anassitos')
-    new_post= Post.create(title='discover new places', content='come and visit this place with your friends', user_id = anas)
 
 
